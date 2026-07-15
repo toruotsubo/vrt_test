a1fc25d18e87dc6c4ce850605b0256aebc91f847
Totu Otsubo <otsubo@tokotan.jp>
2026-07-15 20:07:50 +0900

    wmsサイトデータは不要なので削除

--- choice/js/unique.js
+++
@@ -1,8 +0,0 @@
-$(function(){
-	
-	$('.mainSection .btn').on('click', function(ev){
-		ev.preventDefault();
-		$(this).parent().toggleClass('active').find('.block02').stop().slideToggle(500);
-	});
-	
-});
